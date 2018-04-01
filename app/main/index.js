edb6ec9cc046248d201c4f737afe621cd999bfc0
Sanskar <sansyrox@gmail.com>
2018-04-01 23:18:15 +0530

    Added fullscreen feature

--- app/main/index.js
+++ app/main/index.js
@@ -6,14 +6,16 @@ const mainURL = 'file://' + path.join(__dirname, '../renderer', 'index.html');
 const iconUrl = path.join(__dirname, '../resources', 'tray.png');
 
 const prop = {
-    width: 600,
-    height: 700,
-    index: mainURL,
-    icon: iconUrl
+	width: 600,
+	height: 700,
+	index: mainURL,
+	icon: iconUrl,
+	showDockIcon: true
 };
 
 const mb = menubar(prop);
-
-mb.on('ready', () => {
-console.log('app is ready');
-});
+setTimeout(() => {
+	mb.on('ready', () => {
+		console.log('app is ready');
+	});
+}, 0);
